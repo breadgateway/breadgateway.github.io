c4a62e7a5705a5e481915c1b1a9fa8e8cad23b6d
Bread Gateway <breadgateway@gmail.com>
2020-12-06 14:10:40 +1100

    commit?

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 "use strict";
-//i needd to code something :(
+//The solution is litterally here but whatever
 var selectedBreads = [];
 var correctBreads = ["White", "Bun", "Pizza", 'Wholemeal', "Bagel", "Sourdough", "Brioche", "Cake", "Burger", "Baguette", "Purple", "Sandwich", "Fairy", "Ultra"];
 var breadDropdown = document.getElementById("BreadSelect");
 var selectedBreadsList = document.getElementById("SelectedBreads");
+var feedbackText = document.getElementById("feedback");
 function AddBread() {
     // console.log(;
     selectedBreads.push(breadDropdown.value);
@@ -12,6 +13,33 @@ function AddBread() {
 function RemoveBread() {
     selectedBreads.pop();
     UpdateCards();
+}
+function CheckCombination() {
+    if (selectedBreads.length === correctBreads.length && selectedBreads.every(function (value, index) { return value === correctBreads[index]; })) {
+        feedbackText.innerHTML = "Correct Combination! Gates Opening...";
+        $("#feedback").fadeIn(500).delay(3000).fadeOut(500);
+        setTimeout(function () {
+            //@ts-expect-error
+            document.location = "/page2.html";
+        }, 6000);
+    }
+    else {
+        if (correctBreads.length > selectedBreads.length) {
+            if (selectedBreads.length == 0) {
+                feedbackText.innerHTML = "Its a combination of breads, not just one!";
+            }
+            else {
+                feedbackText.innerHTML = "Too few breads. Keep Trying!";
+            }
+        }
+        else if (correctBreads.length < selectedBreads.length) {
+            feedbackText.innerHTML = "Too many breads. Keep trying!";
+        }
+        else {
+            feedbackText.innerHTML = "Wrong combination. Keep Trying!";
+        }
+        $("#feedback").fadeIn(500).delay(3000).fadeOut(500);
+    }
 }
 function UpdateCards() {
     selectedBreadsList.textContent = '';
--- script.ts
+++ script.ts
@@ -1,10 +1,12 @@
-//i needd to code something :(
+//The solution is litterally here but whatever
 
 let selectedBreads: string[] = [];
 let correctBreads: string[] = ["White", "Bun", "Pizza", 'Wholemeal', "Bagel", "Sourdough", "Brioche", "Cake", "Burger", "Baguette", "Purple", "Sandwich", "Fairy", "Ultra"];
 
 let breadDropdown: HTMLSelectElement = document.getElementById("BreadSelect") as HTMLSelectElement;
 let selectedBreadsList: HTMLOListElement = document.getElementById("SelectedBreads") as HTMLOListElement;
+
+let feedbackText: HTMLParagraphElement = document.getElementById("feedback") as HTMLParagraphElement;
 
 function AddBread() {
     // console.log(;
@@ -17,6 +19,41 @@ function RemoveBread() {
     selectedBreads.pop();
 
     UpdateCards()
+}
+
+function CheckCombination() {
+
+    if (selectedBreads.length === correctBreads.length && selectedBreads.every(function (value, index) { return value === correctBreads[index] })) {
+        feedbackText.innerHTML = "Correct Combination! Gates Opening...";
+        $("#feedback").fadeIn(500).delay(3000).fadeOut(500);
+
+        setTimeout(
+            function () {
+                //@ts-expect-error
+                document.location = "/page2.html";
+            }
+            , 6000);
+
+    } else {
+
+        if (correctBreads.length > selectedBreads.length) {
+
+            if (selectedBreads.length == 0) {
+                feedbackText.innerHTML = "Its a combination of breads, not just one!";
+            } else {
+                feedbackText.innerHTML = "Too few breads. Keep Trying!";
+            }
+
+        } else if (correctBreads.length < selectedBreads.length) {
+
+            feedbackText.innerHTML = "Too many breads. Keep trying!";
+        } else {
+
+            feedbackText.innerHTML = "Wrong combination. Keep Trying!";
+        }
+
+        $("#feedback").fadeIn(500).delay(3000).fadeOut(500);
+    }
 }
 
 function UpdateCards() {
